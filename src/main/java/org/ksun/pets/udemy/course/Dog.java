ddc54980e1da186118c22fb27156eb93c98f6392
Kseniia Andreeva <kseniia.andreeva@netcracker.com>
2021-07-11 21:38:19 +0300

    constructor

--- src/main/java/org/ksun/pets/udemy/course/Dog.java
+++ src/main/java/org/ksun/pets/udemy/course/Dog.java
@@ -1,10 +1,15 @@
 package org.ksun.pets.udemy.course;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println("constructor");
+    }
+
     @Override
     public void say() {
         System.out.println("B");
     }
+
 
     public void init() {
         System.out.println("init");
